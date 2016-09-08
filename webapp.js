d21f35661bd748f7819ed59bd8569c288d7bd89e
Thomas Cho <s.thomas.cho@gmail.com>
2016-09-07 20:52:37 -0700

    Add body-parser

--- webapp.js
+++ webapp.js
@@ -1,7 +1,27 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
 
 app.use(express.static('static'));
+
+var bugdata = [
+	{id : 1, status : "Open", priority : "P1", owner : "Ravan", title : "App crashes on open" },
+	{id : 2, status : "New", priority : "P2", owner : "Eddie", title : "Misaligned border on panel" }
+];
+
+app.get('/api/bugs', function(req, res){
+	res.json(bugdata);
+});
+
+app.use(bodyParser.json());
+app.post('/api/bugs', function(req, res){
+	console.log("Req body:", req.body);
+	var newBug = req.body;
+	newBug.id = bugdata.length + 1;
+	bugdata.push(newBug);
+	res.json(newBug);
+});
 
 var server = app.listen(3000, function() {
 	var port = server.address().port;
